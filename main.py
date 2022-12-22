acbb1b89308890f07260e09c1ebb00a6f9c1723b
GeckoSplat <111183176+GeckoSplat@users.noreply.github.com>
2022-12-22 21:32:02 +0000

    Docstring at line 5

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ from requests import *
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import json
 
-# TODO: this is not a scraping tool. It uses an API key to fetch data
-# Scraping tool for crypto coin prices from coinmarketcap.com API
-
+"""This is a tool to get crypto coin prices from Coinmarketcap.com using an API key to fetch data."""
 
 def query_prices(parameters: dict, headers: dict, URL: str) -> dict:
     """Connects to API and retrieves information"""
@@ -19,7 +17,6 @@ def query_prices(parameters: dict, headers: dict, URL: str) -> dict:
         response = session.get(URL, params=parameters)
         data = json.loads(response.text)
     except (ConnectionError, Timeout, TooManyRedirects) as e:
-        # TODO: Lazy error handling. Can split into individual exceptions
         raise Exception(e)
 
     return data
@@ -32,7 +29,7 @@ def print_prices(query_data: dict):
         # Symbol e.g BTC
         symbol = entry.get("symbol", None)
 
-        # Full string name i.e : Bitcoin
+        # Full string name e.g : Bitcoin
         name = entry.get("name", None)
 
         # Quote holds all price info
@@ -42,7 +39,6 @@ def print_prices(query_data: dict):
             print(f"Could not retrieve price info for: {name} ({symbol})")
             continue
 
-        # TODO: Lazy. Assuming these always exist from here on. Update currency to param
         price = quote["GBP"]["price"]
         percentage_change = quote["GBP"]["percent_change_24h"]
 
@@ -61,15 +57,10 @@ if __name__ == "__main__":
         config = json.load(file)
         API_KEY = config.get("apikey", None)
 
-    if API_KEY is None:
-        # TODO: check if correct error type
-        raise TypeError("Could not retrieve API_KEY")
 
     # Points to the API interface
     URL = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest"
 
-    # TODO: consider having a configuration file or converting to a command line script which takes
-    #       these as optional arguments which replace the defaults here
     parameters = {
         "start": "1",
         "limit": "20",  # change here to display more / less coins . Max 5000
@@ -79,7 +70,6 @@ if __name__ == "__main__":
 
     data = query_prices(parameters, headers, URL)
 
-    # None inside get() is the default but being explicit for learning purposes
     if data.get("data", None) is not None:
         print(f"{50*'-'}\nSource : CoinMarketCap.com\n{50*'-'}")
         print_prices(data.get("data"))
